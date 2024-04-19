3fa9bb913e41e36464b4ddaeb4f450e140dabe4a
Rafael <rafael.mastellini@alunoescolassociais.org.br>
2024-04-19 12:50:09 -0300

    adiciona os arquivos no readme atualizando-os

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const form = document.getElementById('login-form');
- 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
- 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
- 
-    if (email === 'admin' && password === 'senha123') {
-   
-        window.location.href = 'index.html';
-    } else {
-     
-        alert('Email ou senha incorretos.');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+const nameInput = document.getElementById('name');
+if (nameInput) {
+      nameInput.setAttribute('required', true);
     }
-});
+
+    const loginForm = document.getElementById('formLogin');
+
+    loginForm.addEventListener('submit', (event) => {
+
+      if (emailInput.value === '' || passwordInput.value === '') {
+        event.preventDefault();
+        alert('Por favor, preencha todos os campos.');
+      } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value)) {
+        event.preventDefault();
+        alert('Por favor, digite um e-mail válido.');
+      }
+    });
+    function abrirOutraPagina() {
+      window.location.href = "login.html";
+    }
+    
+    function abrirOutraPagina1() {
+      window.location.href = "inicial.html";
+    }
+    function abrirOutraPagina2() {
+      window.location.href = "bem-vindo.html";
+    }
