e71c51a7f7cf83ce65faa485b5ab05514be98c27
Rafael <rafael.mastellini@alunoescolassociais.org.br>
2024-04-19 07:28:26 -0300

    adiciona os arquivos ao readme

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('login-form');
+ 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+ 
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+ 
+    if (email === 'admin' && password === 'senha123') {
+   
+        window.location.href = 'index.html';
+    } else {
+     
+        alert('Email ou senha incorretos.');
+    }
+});
